cf432e5863f975d91b29e58eeb21a2f7eec8dcd4
wizjany <wizjany@gmail.com>
2026-07-20 15:09:46 -0400

    Satisfy the typechecker for env.

---
+++ src/types.d.ts
@@ -0,0 +1,6 @@
+declare module "$env/static/public" {
+    export const PUBLIC_CDN_MAP: string;
+    export const PUBLIC_EXPORTS_CDN: string;
+    export const PUBLIC_RELAY_HOST: string;
+    export const PUBLIC_RELAY_MODULE: string;
+}
